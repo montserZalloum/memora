103168a3a784cc25540dbc4ac9b25cfcfaa25114
montserZalloum <zmontsaer@gmail.com>
2026-01-19 10:26:40 +0000

    remove react project

--- frontend/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// UI Components
-export { JuicyButton } from './JuicyButton';
-export { ProgressBar } from './ProgressBar';
-export { HeartDisplay } from './HeartDisplay';
-export { Layout } from './Layout';
-
-// Stage Renderers
-export { RevealStage } from './stages/RevealStage';
-export { MatchingStage } from './stages/MatchingStage';
